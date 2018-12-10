fc4165299c603eb8f9e16c7726d3192678c30e1a
Dodek69 <dodek@vip.interia.pl>
2018-12-10 19:49:08 +0100

    php

---
+++ php.php
@@ -0,0 +1,11 @@
+<html>   
+    <head>    
+        <title>Php</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-Language" content="en" />
+        <link rel="stylesheet" type="text/css" href="css/php.css">
+    </head>
+    <body>
+        ale przypał
+    </body>
+</html>
